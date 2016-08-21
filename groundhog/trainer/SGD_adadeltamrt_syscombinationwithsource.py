49b7162af5860204f01e7c85281e774e640bb496
Glaceon31 <grit31@126.com>
2016-08-22 00:37:33 +0800

    m

--- groundhog/trainer/SGD_adadeltamrt_syscombinationwithsource.py
+++ groundhog/trainer/SGD_adadeltamrt_syscombinationwithsource.py
@@ -206,12 +206,12 @@ class SGD(object):
         batch['x'] = batch['x']+X
         X = numpy.zeros((batch['x'].shape[0], diffN), dtype='float32')
         batch['x_mask'] = batch['x_mask']+X
-        #print batch['x'].shape
+        
         H = numpy.zeros((batch['h'].shape[0], diffN,batch['h'].shape[1]), dtype='float32')
         batch['h'] = batch['h']+H
         H = numpy.zeros((batch['h_mask'].shape[0], diffN), dtype='float32')
         batch['h_mask'] = batch['h_mask']+H
-
+        print batch['h'].shape
         batch['y'] = Y
         batch['y_mask'] = YM
         batch['b'] = b
