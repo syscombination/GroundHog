dceff9f46ecd6afc60be9953d6c7fe9029de292a
Glaceon31 <grit31@126.com>
2016-08-21 14:28:50 +0800

    empty

--- experiments/nmt/preprocess/preprocess.py
+++ experiments/nmt/preprocess/preprocess.py
@@ -170,6 +170,7 @@ def create_dictionary():
     vocab = {'UNK': 1, '<s>': 0, '</s>': 0}
     for i, (word, count) in enumerate(vocab_count):
         vocab[word] = i + 2
+    vocab['$'] = i+3
     safe_pickle(vocab, args.dictionary)
     return combined_counter, sentence_counts, counters, vocab
 
