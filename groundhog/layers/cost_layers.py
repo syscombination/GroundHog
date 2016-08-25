a20ce2201073ef6021de211fa6fa366b70fd2025
Glaceon31 <grit31@126.com>
2016-08-25 21:56:26 +0800

    b

--- groundhog/layers/cost_layers.py
+++ groundhog/layers/cost_layers.py
@@ -1047,7 +1047,8 @@ class SoftmaxLayer(CostLayer):
         print 'inside softmax:', emb_val.ndim
         if h:
             if h.ndim == 3:
-                h = h[:state_below.shape[0],:,:].reshape([state_below.shape[0], emb_val.shape[1]])
+                h = h.reshape([h.shape[0]*h.shape[1], emb_val.shape[1]])
+            h = h[emb_val.shape[0],emb_val.shape[1]]
             epsilon = 1e-6
             #epsilon = TT.min(emb_val,axis=0)
             emb_val = emb_val*h
