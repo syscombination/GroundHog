2a6f5c83ce8243b0f93247e9a3f57def734c65d2
Glaceon31 <grit31@126.com>
2016-08-27 04:53:31 +0800

    a

--- experiments/nmt/preprocess/changedict.py
+++ experiments/nmt/preprocess/changedict.py
@@ -6,5 +6,6 @@ vocab = cPickle.load(open(sys.argv[1]))
 for i in vocab:
 	if vocab[i] == string.atoi(sys.argv[3])-1:
 		del vocab[i]
+		break
 vocab['$'] = string.atoi(sys.argv[3])-1
 cPickle.dump(vocab, sys.argv[2], protocol=cPickle.HIGHEST_PROTOCOL)
