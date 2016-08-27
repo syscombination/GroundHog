6a5159092a7451b76bd8844369027b1d321dddf8
Glaceon31 <grit31@126.com>
2016-08-27 19:26:47 +0800

    shuffle

---
+++ experiments/nmt/preprocess/shuffle-json.py
@@ -0,0 +1,33 @@
+import json
+import sys
+import random
+
+source = json.loads(open(sys.argv[1], 'r').read())
+target = json.loads(open(sys.argv[2], 'r').read())
+hypo = json.loads(open(sys.argv[3], 'r').read())
+
+length = len(source)
+print length
+assert(len(target)) == length
+assert(len(hypo)) == length
+
+idx = random.shuffle(range(length))
+newsource = []
+newtarget = []
+newhypo = []
+
+for i in xrange(len(idx)):
+	index = idx[i]
+	newsource.append(source[index])
+	newtarget.append(target[index])
+	newhypo.append(hypo[index])
+
+sourceout = open(sys.argv[1]+'.shuf', 'w')
+sourceout.write(json.dumps(newsource))
+sourceout.close()
+targetout = open(sys.argv[2]+'.shuf', 'w')
+targetout.write(json.dumps(newtarget))
+targetout.close()	
+hypoout = open(sys.argv[3]+'.shuf', 'w')
+hypoout.write(json.dumps(newhypo))
+hypoout.close()
