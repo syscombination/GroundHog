80690838d248d5f2fabad058f25263f4ac62a830
Glaceon31 <grit31@126.com>
2016-08-27 17:04:44 +0800

    a

--- groundhog/trainer/SGD_adadeltamrt_scws.py
+++ groundhog/trainer/SGD_adadeltamrt_scws.py
@@ -211,7 +211,9 @@ class SGD(object):
         batch['x_mask'] = batch['x_mask']+X
         
         H = numpy.zeros((diffN,batch['h'].shape[0], batch['h'].shape[1]), dtype='float32')
-        batch['h'] = (batch['h'].dimshuffle(1,0,2)+H).dimshuffle(1,0,2)
+        tmph = batch['h'].reshape(batch['h'].shape[1],batch['h'].shape[0],batch['h'].shape[2])
+        batch['h'] = tmph+H
+        batch['h'] = batch['h'].reshape(batch['h'].shape[1],batch['h'].shape[0],batch['h'].shape[2])
         H = numpy.zeros((batch['h_mask'].shape[0], diffN), dtype='float32')
         batch['h_mask'] = batch['h_mask']+H
         print 'output x shape:',batch['x'].shape
