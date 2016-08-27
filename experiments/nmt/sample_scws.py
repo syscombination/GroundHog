08a03b39ef0148b6142bf8bbc99a18e327935500
Glaceon31 <grit31@126.com>
2016-08-27 12:42:02 +0800

    a

--- experiments/nmt/sample_scws.py
+++ experiments/nmt/sample_scws.py
@@ -76,7 +76,9 @@ class BeamSearch(object):
             last_words = (numpy.array(map(lambda t : t[-1], trans))
                     if k > 0
                     else numpy.zeros(beam_size, dtype="int64"))
-            log_probs = numpy.log(self.comp_next_probs(c, h0, k, last_words, *states)[0])
+            probs = self.comp_next_probs(c, h0, k, last_words, *states)[0]
+            print probs
+            log_probs = numpy.log(probs)
             print last_words
             print log_probs
 
