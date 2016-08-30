d93ef12517fa6323ef0a7081e5f1af2816517a06
Glaceon31 <grit31@126.com>
2016-08-31 00:14:58 +0800

    a

---
+++ experiments/nmt/preprocess/delempty.py
@@ -0,0 +1,12 @@
+import codecs
+import string
+import sys
+import copy
+
+inpfile = sys.argv[1]
+inp = open(inpfile, 'r').read()
+inp = inp.replace('$ ', '')
+
+output = open(sys.argv[2],'w')
+output.write(inp)
+output.close()
