c770cc21d681b00c435fe22de1a2881f7437f9c4
Glaceon31 <grit31@126.com>
2016-09-11 12:37:21 +0800

    a

--- experiments/nmt/sample_la.py
+++ experiments/nmt/sample_la.py
@@ -70,7 +70,7 @@ class BeamSearch(object):
 
         for k in range(len(systems)):
             print '-----',k,'-----'
-            print 'trans:,'trans
+            print 'trans:',trans
             if n_samples == 0:
                 break
 
